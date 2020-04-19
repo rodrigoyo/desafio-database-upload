db5dcf104c9cfad15e476edd21909e3b26c9ae20
Rodrigo Yoshioka <rodrigo.yoshioka@gmail.com>
2020-04-19 19:53:14 -0300

    Add delete transactions routine

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -3,7 +3,7 @@ import { getCustomRepository } from 'typeorm';
 
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import CreateTransactionService from '../services/CreateTransactionService';
-// import DeleteTransactionService from '../services/DeleteTransactionService';
+import DeleteTransactionService from '../services/DeleteTransactionService';
 // import ImportTransactionsService from '../services/ImportTransactionsService';
 
 const transactionsRouter = Router();
@@ -32,6 +32,13 @@ transactionsRouter.post('/', async (request, response) => {
 
 transactionsRouter.delete('/:id', async (request, response) => {
   // TODO
+  const { id } = request.params;
+
+  const deleteTransactionService = new DeleteTransactionService();
+
+  await deleteTransactionService.execute(id);
+
+  response.status(204).send();
 });
 
 transactionsRouter.post('/import', async (request, response) => {
