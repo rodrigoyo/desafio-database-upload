f8f4ef5a686f5b32e01afec8e29935bdd58bcc90
Rodrigo Yoshioka <rodrigo.yoshioka@gmail.com>
2020-04-19 15:58:30 -0300

    Other solution to getBalance

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -13,13 +13,34 @@ class TransactionsRepository extends Repository<Transaction> {
   public async getBalance(): Promise<Balance> {
     // TODO
     const transactions = await this.find();
-    const income = transactions
-      .filter(item => item.type === 'income')
-      .reduce((acc, item) => acc + Number(item.value), 0);
+    // const income = transactions
+    //   .filter(item => item.type === 'income')
+    //   .reduce((acc, item) => acc + Number(item.value), 0);
 
-    const outcome = transactions
-      .filter(item => item.type === 'outcome')
-      .reduce((acc, item) => acc + Number(item.value), 0);
+    // const outcome = transactions
+    //   .filter(item => item.type === 'outcome')
+    //   .reduce((acc, item) => acc + Number(item.value), 0);
+
+    const { income, outcome } = transactions.reduce(
+      (acc, item) => {
+        switch (item.type) {
+          case 'income':
+            acc.income += Number(item.value);
+            break;
+          case 'outcome':
+            acc.outcome += Number(item.value);
+            break;
+          default:
+            break;
+        }
+
+        return acc;
+      },
+      {
+        income: 0,
+        outcome: 0,
+      },
+    );
 
     const total = income - outcome;
 
