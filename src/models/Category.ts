b4ea1bd7dbbf03cf76f0a0dc1a1049d0c5d9e389
Rodrigo Yoshioka <rodrigo.yoshioka@gmail.com>
2020-04-19 10:36:51 -0300

    Add migration with categories

--- src/models/Category.ts
+++ src/models/Category.ts
@@ -1,10 +1,16 @@
+import { Column, CreateDateColumn, PrimaryGeneratedColumn } from 'typeorm';
+
 class Category {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
   title: string;
 
+  @CreateDateColumn()
   created_at: Date;
 
+  @CreateDateColumn()
   updated_at: Date;
 }
 
